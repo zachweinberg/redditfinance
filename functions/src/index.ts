5e85f4b6358bf54a58176bc008da1d24fd5f53b8
Zach Weinberg <zweinberg3@gmail.com>
2022-01-01 21:44:36 -0500

    refactor: add http trigger

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,54 +11,65 @@ export const scrapeSubreddits = functions
   .runWith({ timeoutSeconds: 240, memory: '2GB' })
   .pubsub.schedule('0 */3 * * *')
   .timeZone('America/New_York')
-  .onRun(async (context) => {
-    console.log('-- STARTING REDDIT SCRAPE -- ')
-
-    for (const sub of subredditList) {
-      try {
-        const info = await getSubredditInfo(sub.name)
-
-        const name = decode(info.data.display_name)
-        const url = info.data.url
-        const subscribers = info.data.subscribers
-        const foundedAt = info.data.created * 1000
-
-        const title = info.data.title ?? null
-        const description = info.data.public_description ?? info.data.description ?? null
-
-        const logo = info.data.community_icon
-          ? decode(info.data.community_icon)
-          : info.data.icon_img
-          ? decode(info.data.icon_img)
-          : null
-
-        console.log(` -- SAVING ${sub.name} to firestore --`)
-
-        const data: Partial<Subreddit> = {
-          name,
-          url,
-          subscribers,
-          foundedAt,
-          title,
-          redditDescription: description,
-          logo,
-          updatedAt: Date.now(),
-          tags: sub.tags ?? [],
-        }
-
-        await admin
-          .firestore()
-          .collection('subreddits')
-          .doc(sub.name)
-          .set(data, { merge: true })
-      } catch (e) {
-        console.error(e)
-      }
-    }
-
-    const deployHookURL = functions.config().vercel.deploy_hook
-
-    await axios.post(deployHookURL)
-
-    return true
+  .onRun(async (ctx) => {
+    await startScrape()
+    await deployWebsite()
   })
+
+export const httpTriggerScrape = functions.https.onRequest(async (req, res) => {
+  await startScrape()
+  await deployWebsite()
+})
+
+const deployWebsite = async () => {
+  const deployHookURL = functions.config().vercel.deploy_hook
+  await axios.post(deployHookURL)
+}
+
+const startScrape = async () => {
+  console.log('-- STARTING REDDIT SCRAPE -- ')
+
+  for (const sub of subredditList) {
+    try {
+      const info = await getSubredditInfo(sub.name)
+
+      const name = decode(info.data.display_name)
+      const url = info.data.url
+      const subscribers = info.data.subscribers
+      const foundedAt = info.data.created * 1000
+
+      const title = info.data.title ?? null
+      const description = info.data.public_description ?? info.data.description ?? null
+
+      const logo = info.data.community_icon
+        ? decode(info.data.community_icon)
+        : info.data.icon_img
+        ? decode(info.data.icon_img)
+        : null
+
+      console.log(` -- SAVING ${sub.name} to firestore --`)
+
+      const data: Partial<Subreddit> = {
+        name,
+        url,
+        subscribers,
+        foundedAt,
+        title,
+        redditDescription: description,
+        logo,
+        updatedAt: Date.now(),
+        tags: sub.tags ?? [],
+      }
+
+      await admin
+        .firestore()
+        .collection('subreddits')
+        .doc(sub.name)
+        .set(data, { merge: true })
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  return true
+}
