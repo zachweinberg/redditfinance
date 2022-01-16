1dedea28441b4ab7cef17e921b8f7de9dd80822f
Zach Weinberg <39999870+zachweinberg@users.noreply.github.com>
2022-01-16 12:08:07 -0500

    feat: add footer (#1)

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer: React.FunctionComponent = () => {
+  return (
+    <footer className="flex items-center justify-center py-10 mt-16">
+      <a
+        href="https://github.com/zachweinberg/redditfinance"
+        target="_blank"
+        className="text-sm text-gray-400 hover:text-gray-600"
+      >
+        This website is open source
+      </a>
+    </footer>
+  )
+}
+
+export default Footer
